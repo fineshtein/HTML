3b6f9b255daf5513905c5d096ce4e2e68a7063b9
fineshtein <62895720+fineshtein@users.noreply.github.com>
2020-05-31 18:03:11 +0300

    Сделал немного попроще

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,33 @@
-document.getElementById('inpt').oninput = borderRadius;
-let divBorder = document.getElementById('border');
+const border = document.querySelector('.border');
 
-function borderRadius() {
-   divBorder.style.borderRadius = this.value + 'px';
+inptWidth.oninput = function () {
+   let inptWidth = document.querySelector('.inptWidth');
+   border.style.width = this.value + 'px';
+}
+
+inptHeight.oninput = function () {
+   let inptHeight = document.querySelector('.inptHeight');
+   border.style.height = this.value + 'px';
+}
+
+radius.oninput = function () {
+   let radius = document.querySelector('.radius');
+   border.style.borderRadius = this.value + 'px';
+}
+
+rotation.oninput = function () {
+   let rotation = document.querySelector('.rotation');
+   border.style.transform = `rotate(${this.value}deg)`;
+}
+
+wordFontSize.oninput = function () {
+   let wordFontSize = document.querySelector('.wordFontSize');
+   border.style.fontSize = this.value + 'px';
 }
 
 document.querySelector('.button').onclick = showWord;
 
 function showWord() {
    let word = document.querySelector('.input2').value;
-   divBorder.textContent = word;
+   border.textContent = word;
 }
-
-document.getElementById('wordFontSize').oninput = wordFontSize;
-
-function wordFontSize() {
-   let font = document.getElementById('wordFontSize').value;
-   divBorder.style.fontSize = font + 'px';
-}
-
-document.getElementById('inptWidth').oninput = changeWidth;
-
-function changeWidth() {
-   let divWidth = document.getElementById('inptWidth').value;
-   divBorder.style.width = divWidth + 'px';
-}
-
-document.getElementById('inptHeight').oninput = changeHeight;
-
-function changeHeight() {
-   let divHeight = document.getElementById('inptHeight').value;
-   divBorder.style.height = divHeight + 'px';
-}
-
-document.getElementById('rotation').oninput = rotateDiv;
-
-function rotateDiv() {
-   let rotateDiv = document.getElementById('rotation').value;
-   divBorder.style.transform = 'rotate(' + rotateDiv + 'deg)';
-}
-
